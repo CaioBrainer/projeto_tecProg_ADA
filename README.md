7bfeccde385c35a5f24dc7f4b45473e5b64a7baa
Caio Brainer <caiocgbrainer@gmail.com>
2024-03-15 13:25:09 -0300

    atualização

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# projeto_tecProg_ADA
+# PROJETO ADA
+## MÓDULO: TÉCNICAS DE PROGRAMAÇÃO II
+### Integrantes do Grupo: Caio Brainer, Thaisa Elvas, Alex Amaro, Luana Rodrigues, Lucindo e Henricco
+
+<p align:"justify"> Neste projeto proposto como finalização do módulo d técnicas de programação do curso de Engenharia de Dados - SantanderCoders 2023.2, nós fomos orientados a selecionar uma base de dados e aplicar ténicas de EDA (Exploratory Data Analisys ou Análise Exploratória de Dados) e elaborar uma apresentação para expor informações importantes extraidas dos dados.</p>
+<p> Durante o projeto, cada integrante do grupo ficou responsável por elaborar uma "pergunta de négocio" e  a respondê-la (utilizando a biblioteca pandas), e ao término da análise de todos foi elaborada uma apresentação utilizando a biblioteca streamlit.</p>
+
+## EXECUÇÃO DA APRESENTAÇÃO
+
+<p>Para executarmos o apresentação, é preciso ter a biblioteca streamlit instalada no sistema e executar o comando: </p>
+
+```streamlit run apresentacao.py```
+
+<p>na pasta onde se encontra os aqruivos do projeto.</p>
--- apresentacao.py
+++ apresentacao.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 import plotly.io as pio
 import json
 
@@ -17,6 +17,26 @@ tipo_pagamento = base_ecomerce.groupby(by='payment_type').size().reset_index().s
 tipo_pagamento = tipo_pagamento.rename(columns={'payment_type': 'forma de pagamento', 0: 'quantidade'})
 mapeamento = {'credit_card': 'crédito', 'debit_card': 'débito', 'voucher':'voucher', 'boleto':'boleto'}
 tipo_pagamento['forma de pagamento'] = tipo_pagamento['forma de pagamento'].map(mapeamento)
+
+# Código da Luana
+clientes_por_estado = base_ecomerce.groupby('customer_state')['customer_id'].nunique().sort_values(
+    ascending=False).reset_index()
+media_frete_por_estado = base_ecomerce.groupby('customer_state')['freight_value'].mean().sort_values(
+    ascending=False).reset_index()
+vendedores_por_estado = base_ecomerce.groupby('seller_state')['seller_id'].nunique().sort_values(
+    ascending=False).reset_index()
+
+merged_df = clientes_por_estado.merge(media_frete_por_estado, on='customer_state')
+merged_df = merged_df.merge(vendedores_por_estado, how='outer', left_on='customer_state', right_on='seller_state')
+merged_df.rename(columns={'customer_id': 'clientes_unicos', 'freight_value': 'valor_medio_frete', 'seller_id': 'num_vendedores'}, inplace=True)
+merged_df = merged_df.sort_values(by='clientes_unicos', ascending=False)
+
+# Código Alex Amaro
+base_ecomerce['order_purchase_timestamp'] = pd.to_datetime(base_ecomerce['order_purchase_timestamp'])
+# Create a new column for the hour of the day
+base_ecomerce['hour_of_day'] = base_ecomerce['order_purchase_timestamp'].dt.hour
+# Grouping by hour of the day
+sales_by_hour = base_ecomerce.groupby('hour_of_day').size().reset_index(name='count')
 
 # Título da página
 #st.markdown("<h1 style='text-align: center; color: black;'>Diagramas\n</h1> <br>", unsafe_allow_html=True)
@@ -76,15 +96,76 @@ if opcoes == 'Gráficos':
     st.plotly_chart(fig_map)
     st.plotly_chart(fig_bar)
     st.plotly_chart(fig_forma)
+
     
+    st.markdown("<h3 style='text-align: center; color: black;'>Qual a relação entre o número de clientes, \n"
+    "vendedores e o valor médio do frete para cada estado?\n</h2> <br>",unsafe_allow_html=True)
+
+    st.code("df_unido_amostra = pd.read_parquet('data/amostra.parquet')\n"
+        "clientes_por_estado = df_unido_amostra.groupby('customer_state')['customer_id'].nunique().sort_values(ascending=False).reset_index()\n"
+        "media_frete_por_estado = df_unido_amostra.groupby('customer_state')['freight_value'].mean().sort_values(ascending=False).reset_index()\n"
+        "vendedores_por_estado = df_unido_amostra.groupby('seller_state')['seller_id'].nunique().sort_values(ascending=False).reset_index()\n"
+        "merged_df = clientes_por_estado.merge(media_frete_por_estado, on='customer_state')\n"
+        "merged_df = merged_df.merge(vendedores_por_estado, how='outer', left_on='customer_state', right_on='seller_state')")
+
+    # Figura Luana
+    fig_frete = go.Figure()    
+    # Gráfico de barras
+    fig_frete.add_trace(go.Bar(
+        x=merged_df['customer_state'],
+        y=merged_df['clientes_unicos'],
+        name='Número de Clientes',
+        marker_color='blue'
+    ))
+    
+    fig_frete.add_trace(go.Bar(
+        x=merged_df['customer_state'],
+        y=merged_df['num_vendedores'],
+        name='Número de Vendedores',
+        marker_color='red'
+    ))
+    
+    # Gráfico de linhas
+    fig_frete.add_trace(go.Scatter(
+        x=merged_df['customer_state'],
+        y=merged_df['valor_medio_frete'],
+        mode='lines+markers',
+        name='Valor Médio do Frete',
+        yaxis='y2',
+        marker=dict(color='green'),
+        line=dict(dash='dash')
+    ))
+    
+    fig_frete.update_layout(
+        title='Quantidade de Clientes, Vendedores e Valor Médio do Frete por Estado',
+        xaxis=dict(title='Estados', tickangle=35),
+        yaxis=dict(title='Número de Clientes e Vendedores', side='left', showgrid=False),
+        yaxis2=dict(title='Valor Médio do Frete', overlaying='y', side='right', showgrid=False),
+        legend=dict(x=0, y=1.1, traceorder="normal")
+    )
+
+    st.plotly_chart(fig_frete)
+
+    # Figura Alex Amaro
+    st.markdown("<h3 style='text-align: center; color: black;'>Qual a quantidade de vendas por Hora do Dia\n</h2> <br>",
+                unsafe_allow_html=True)
+    st.code("base_ecomerce['order_purchase_timestamp'] = pd.to_datetime(base_ecomerce['order_purchase_timestamp'])\n"
+            "base_ecomerce['hour_of_day'] = base_ecomerce['order_purchase_timestamp'].dt.hour\n"
+            "sales_by_hour = base_ecomerce.groupby('hour_of_day').size().reset_index(name='count')")
+
+    fig = px.bar(sales_by_hour, x='hour_of_day', y='count',
+                 labels={'hour_of_day': 'Hora do Dia', 'count': 'Quantidade de Vendas'},
+                 color='count')
+    fig.update_layout(xaxis=dict(tickmode='linear', tick0=0, dtick=1))
+    st.plotly_chart(fig, use_container_width=True)
 
     # st.caption("<p style='text-align: center; color: black;'>vai rpecisar de legenda?\n</p> <br>",
     #           unsafe_allow_html=True)
 
 
 # Opção 2: Dados Tabelados
-elif opcoes == 'Dados Tabelados':
-    st.subheader('Dados Tabelados')
 
-    # Exibindo dados em tabela
+elif opcoes == "Dados Tabelados\n":
+
+    st.subheader("Dados Tabelados")
     st.write(compra_por_estado.head(10))
